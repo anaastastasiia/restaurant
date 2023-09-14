c5c4330a71ba9ebbef25dbc16afed421caf1a6ac
anaastastasiia <nastiaproturenko5@gmail.com>
2023-09-14 14:39:34 +0200

    added db.json and ProductItem component

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import './MainPage.modue.scss';
+import ProductItem from '../../components/ProductItem';
 
 export const MainPage = () => {
   return (
     <>
       <div className="wrapper">
-        <div className="celebrate">Celebrate Your Senses</div>
-        <div className="logo">RADISH</div>
-        <div className="restaurant">RESTAURANT</div>
+        <div className="infoWrapper">
+          <div className="celebrate">Celebrate Your Senses</div>
+          <div className="logo">RADISH</div>
+          <div className="restaurant">RESTAURANT</div>
+        </div>
+        <a href="./#showMore" className="showMoreLink">
+          Pokaż więcej
+        </a>
+      </div>
+      <div id="showMore">
+        <div className="contentWrapper">
+          <h1>Popularne dania</h1>
+          <div className="content">
+            {/* <h1>Nowa zawartość</h1> */}
+            {/* <p>Tutaj jest więcej treści...</p> */}
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+          </div>
+        </div>
       </div>
     </>
   );
