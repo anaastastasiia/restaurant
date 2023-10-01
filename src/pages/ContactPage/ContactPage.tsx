83b502ec530e4ca2cadb5d8944ee5018b0fc6c72
anaastastasiia <nastiaproturenko5@gmail.com>
2023-10-01 15:14:59 +0200

    added icons and styles for contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,35 +1,28 @@
 import styles from './ContactPage.module.scss';
-import React from 'react';
-import GoogleMapReact from 'google-map-react';
+import { useEffect, useState } from 'react';
+import { getDay } from 'date-fns';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import PhoneAndroidOutlinedIcon from '@mui/icons-material/PhoneAndroidOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import AccessTimeOutlinedIcon from '@mui/icons-material/AccessTimeOutlined';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined';
 
-interface MapProps {
-  center: { lat: number; lng: number };
-  zoom: number;
-}
-
-const Map: React.FC<MapProps> = ({ center, zoom }) => {
-  return (
-    <div style={{ height: '400px', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: '' }} // Zastąp 'TWÓJ_KLUCZ_API' własnym kluczem API Google Maps
-        defaultCenter={center}
-        defaultZoom={zoom}
-      >
-        {/* Dodaj marker na mapie */}
-        <AnyReactComponent lat={center.lat} lng={center.lng} text="Miejsce" />
-      </GoogleMapReact>
-    </div>
-  );
-};
-const AnyReactComponent: React.FC<{
-  lat: number;
-  lng: number;
-  text: string;
-}> = ({ text }) => <div>{text}</div>;
-//<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2468.6685399890134!2d19.46844629999999!3d51.77566645000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bcb265106e829%3A0xf21167d962131d1f!2sAkademia%20Humanistyczno-Ekonomiczna%20w%20%C5%81odzi!5e0!3m2!1spl!2spl!4v1695572456182!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 export const ContactPage = () => {
-  const center = { lat: 51.774969, lng: 19.469697 }; // Współrzędne centrum mapy
-  const zoom = 11; // Poziom powiększenia
+  const [currentDay, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    const currentDate = new Date();
+    const dayOfWeek = getDay(currentDate);
+    if (dayOfWeek === 0 || dayOfWeek === 6) {
+      setCurrentDate('weekend');
+    }
+  }, []);
+  const [isExpanded, setExpanded] = useState(false);
+
+  const handleToggle = () => {
+    setExpanded(!isExpanded);
+  };
   return (
     <>
       <div className={styles.wrapper}>
@@ -43,14 +36,78 @@ export const ContactPage = () => {
           <div className={styles.content}>
             <div>Skontaktuj się z nami</div>
           </div>
+          <div className={styles.contactIconsWrapper}>
+            <div
+              className={styles.contactIcon}
+              style={{ borderRight: '1px solid #6d1414', width: '33%' }}
+            >
+              <LocationOnOutlinedIcon
+                style={{ color: 'white', width: ' 40px', height: ' 40px' }}
+              />
+              <div>Nasz adres</div>
+              <div className={styles.textBold}>
+                Doktora Seweryna Sterlinga 26, 90-212 Łódź
+              </div>
+            </div>
+            <div
+              className={styles.contactIcon}
+              style={{ borderRight: '1px solid #6d1414', width: '33%' }}
+            >
+              <PhoneAndroidOutlinedIcon
+                style={{ color: 'white', width: ' 40px', height: ' 40px' }}
+              />
+              <div>Numer kontaktowy</div>
+              <div className={styles.textBold}>+(48) 444 555 444</div>
+            </div>
+            <div
+              className={styles.contactIcon}
+              style={{ borderRight: '1px solid #6d1414', width: '33%' }}
+            >
+              <EmailOutlinedIcon
+                style={{ color: 'white', width: ' 40px', height: ' 40px' }}
+              />
+              <div>Adres email</div>
+              <div className={styles.textBold}>radish@gmail.com</div>
+            </div>
+            <div className={styles.contactIcon} style={{ width: '33%' }}>
+              <AccessTimeOutlinedIcon
+                style={{ color: 'white', width: ' 40px', height: ' 40px' }}
+              />
+              <div>Godziny otwarcia</div>
+              <div className={styles.houres}>
+                <div>
+                  Dzisiaj:{' '}
+                  {currentDay === 'weekend' ? '9:00 - 24:00' : '11:00 - 23:00'}
+                </div>
+                <div onClick={handleToggle} style={{ cursor: 'pointer' }}>
+                  {isExpanded ? (
+                    <ArrowDropUpOutlinedIcon />
+                  ) : (
+                    <ArrowDropDownOutlinedIcon />
+                  )}
+                </div>
+              </div>
+              {isExpanded && (
+                <div>
+                  <div>poniedziałek 11:00 - 23:00</div>
+                  <div>wtorek 11:00 - 23:00</div>
+                  <div>środa 11:00 - 23:00</div>
+                  <div>czwartek 11:00 - 23:00</div>
+                  <div>piątek 11:00 - 23:00</div>
+                  <div>sobota 9:00 - 24:00</div>
+                  <div>niedziela 9:00 - 24:00</div>
+                </div>
+              )}
+            </div>
+          </div>
+
           <div className={styles.mapWrapper}>
-            <div>Jak do nas dojechać</div>
+            <div>Mapa dojazdu</div>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2468.6685399890134!2d19.46844629999999!3d51.77566645000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bcb265106e829%3A0xf21167d962131d1f!2sAkademia%20Humanistyczno-Ekonomiczna%20w%20%C5%81odzi!5e0!3m2!1spl!2spl!4v1695572456182!5m2!1spl!2spl"
               width="900"
               height="300"
             ></iframe>
-            {/* <Map center={center} zoom={zoom} /> */}
           </div>
         </div>
       </div>
