aec80eb7937c34e8ecedf8ba5236f39b3fdcaf05
anaastastasiia <nastiaproturenko5@gmail.com>
2023-10-07 14:20:31 +0200

    added scrolling images to main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,6 +4,11 @@ import ProductItem from '../../components/ProductItem';
 import { CustomLink } from '../../components/CustomLink';
 import './MainPage.modue.scss';
 import { ContactUs } from '../../components/ContactUs';
+import Rectangle9 from '../../assets/Rectangle9.png';
+import Rectangle8 from '../../assets/Rectangle8.png';
+import Rectangle11 from '../../assets/Rectangle11.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from 'react-bootstrap';
 
 export const MainPage = () => {
   const { getItems } = useItemsActions;
@@ -12,6 +17,8 @@ export const MainPage = () => {
   useEffect(() => {
     getItems();
   }, []);
+
+  const images = [Rectangle9, Rectangle8, Rectangle11];
 
   return (
     <>
@@ -25,17 +32,31 @@ export const MainPage = () => {
           <a href="./#showMore">Pokaż więcej</a>
         </div>
       </div>
-      <div>
+      <div className="contactUsWrapper">
         <ContactUs />
       </div>
+
       <div id="showMore">
         <div className="contentWrapper">
           <h1>Popularne dania</h1>
-          <div className="content">
+          <div className="scrollWrapper text-center">
+            <Carousel style={{ maxWidth: '600px', margin: 'auto' }}>
+              {images.map((image, index) => (
+                <Carousel.Item key={index}>
+                  <img
+                    className="d-block mx-auto" // Dodano klasę mx-auto
+                    src={image}
+                    alt={`Slide ${index + 1}`}
+                  />
+                </Carousel.Item>
+              ))}
+            </Carousel>
+          </div>
+          {/* <div className="content">
             {item.map((i) => {
               return <ProductItem name={i.name} price={i.price} id={i.id} />;
             })}
-          </div>
+          </div> */}
           <div className="menuLinkWrapper">
             <CustomLink to="/menu">Przejdź do menu</CustomLink>
           </div>
