11da0506bf7a02e4398a989d92c6c3bb552ba5f6
anaastastasiia <nastiaproturenko5@gmail.com>
2023-10-21 12:32:29 +0200

    fixed styles for map wrapper on contact page

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -12,14 +12,14 @@ export const ContactPage = () => {
       </div>
       <div className={styles.contactWithMap}>
         <ContactUs />
-        <div className={styles.mapWrapper}>
-          <div>Mapa dojazdu</div>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2468.6685399890134!2d19.46844629999999!3d51.77566645000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bcb265106e829%3A0xf21167d962131d1f!2sAkademia%20Humanistyczno-Ekonomiczna%20w%20%C5%81odzi!5e0!3m2!1spl!2spl!4v1695572456182!5m2!1spl!2spl"
-            width="900"
-            height="300"
-          ></iframe>
-        </div>
+      </div>
+      <div className={styles.mapWrapper}>
+        <div>Mapa dojazdu</div>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2468.6685399890134!2d19.46844629999999!3d51.77566645000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x471bcb265106e829%3A0xf21167d962131d1f!2sAkademia%20Humanistyczno-Ekonomiczna%20w%20%C5%81odzi!5e0!3m2!1spl!2spl!4v1695572456182!5m2!1spl!2spl"
+          width="900"
+          height="300"
+        ></iframe>
       </div>
     </>
   );
