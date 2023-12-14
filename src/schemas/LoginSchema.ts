dd35c417e8f810e97d5e9495bc855424d9be081d
anaastastasiia <nastiaproturenko5@gmail.com>
2023-12-14 20:27:57 +0100

    validation for login form

--- src/schemas/LoginSchema.ts
+++ src/schemas/LoginSchema.ts
@@ -2,7 +2,12 @@ import * as yup from 'yup';
 
 const loginSchema = (t: any) => yup.object().shape({
   username: yup.string().required(t('validationErrors.required')),
-  password: yup.string().required(t('validationErrors.required')).min(8, t('validationErrors.min8')),
+  password: yup    
+    .string().required(t('validationErrors.required'))
+    .min(8, t('validationErrors.min8'))
+    .matches(/[A-Z]/, t('validationErrors.uppercase'))
+    .matches(/[!@#$%^&*(),.?":{}|<>]/, t('validationErrors.specialChar'))
+    .matches(/\S/, t('validationErrors.noSpace')),
 });
 
 export default loginSchema;
